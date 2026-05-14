dd82cba04144395fe33bd9e3a349439ff825e1f9
startone-1 <155465826+startone-1@users.noreply.github.com>
2026-05-14 18:27:04 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# LINE公式アカウントの設定（ここはRenderの環境変数で設定します）
+# LINE公式アカウントの設定（Render.comで環境変数として設定します）
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
-# Groq APIキー（ここもRenderの環境変数で設定します）
+# Groq APIキー（Render.comで環境変数として設定します）
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 # アプリ設定
